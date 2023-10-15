e4ee17e9ffe8de7bf544d72dcf8ac3518326ba6c
SwitU7Ronald <c2tu6u@gmail.com>
2023-10-15 10:51:53 +0530

    Categories Image Arranged - Assign Image Of Categories According To Categories Name

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,22 +1,47 @@
 import 'package:emart_app/consts/consts.dart';
 
-const socialIconList = [icFacebookLogo,icGoogleLogo,icTwitterLogo]; 
+const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
 
-const slidersList = [imgSlider1,imgSlider2,imgSlider3,imgSlider4];
-const secoundSlidersList = [imgSs1,imgSs2,imgSs3,imgSs4];
+const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+const secoundSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
 
-const featuredImages1 = [imgS1,imgS2,imgS3];
-const featuredImages2 = [imgS4,imgS5,imgS6];
+const featuredImages1 = [imgS1, imgS2, imgS3];
+const featuredImages2 = [imgS4, imgS5, imgS6];
 
-const featuredTitles1 = [womenDress,girlsDress,girlsWatches];
-const featuredTitles2 = [boysGlasses,mobilePhone,tShirts];
+const featuredTitles1 = [womenDress, girlsDress, girlsWatches];
+const featuredTitles2 = [boysGlasses, mobilePhone, tShirts];
 
 const categoriesList = [
-  womenDress,menClothingFashion,compAccess,automobile,kidtoys,sports,jewelery,cellphone,furnitur];
+  womenDress,
+  menClothingFashion,
+  compAccess,
+  automobile,
+  kidtoys,
+  sports,
+  jewelery,
+  cellphone,
+  furnitur
+];
 
-const categoriesImages = [imgFc1,imgFc2,imgFc3,imgFc4,imgFc5,imgFc6,imgFc7,imgFc8,imgFc9];
+const categoriesImages = [
+  imgS1,
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+  imgS11,
+  imgFc6,
+  imgFc9
+];
 
-const itemDetailButtonList = [video,reviews,privacyPolicy,returnpolicy,supportPolicy];
+const itemDetailButtonList = [
+  video,
+  reviews,
+  privacyPolicy,
+  returnpolicy,
+  supportPolicy
+];
 
-const profileButtonsList = [orders,wishlist,messagse];
+const profileButtonsList = [orders, wishlist, messagse];
 const profileButtonsIcon = [icOrder, icOrders, icMessages];
